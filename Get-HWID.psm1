472bcff7bc88f6fd56a64df4a119afb5b0bfaca0
Calvin <74103103+Calvindd2f@users.noreply.github.com>
2023-07-05 18:59:07 +0100

    Update Get-HWID.psm1

--- Get-HWID.psm1
+++ Get-HWID.psm1
@@ -1,67 +1,68 @@
+###[System]###
+$system = Get-WmiObject Win32_ComputerSystemProduct
+$bios = Get-WmiObject Win32_BIOS
+$baseboard = Get-WmiObject Win32_BaseBoard
+###[Drives]###
+$drives = Get-WmiObject Win32_DiskDrive
+###[Network Adapter]###
+$adapters = Get-NetAdapter
+###[Physical RAM]###
+$memoryModules = Get-WmiObject Win32_PhysicalMemory
+###[Power Supply]###
+#####################################################
+
 $multiLine = @"
-Calvindd2f | Get-HWID 
+$(Write-Host "Calvindd2f | Get-HWID")
 
-System
-======================================================================
-BIOS UUID:               
-BIOS Serial:             System Serial Number
-Baseboard Serial:        
-Baseboard Asset Tag:     Default string
-Enclosure Serial:        Default string
-Enclosure Asset Tag:     Default string
-
-
-Disk (HDD/SSD/NVMe)
-======================================================================
-Model:                   
-Serial number:                       
-----------------------------------------------------------------------
-Model:                   
-Serial number:           
-----------------------------------------------------------------------
-Model:                   
-Serial number:                       
-----------------------------------------------------------------------
-Model:                   
-Serial number:           
-----------------------------------------------------------------------
+$( 
+  Write-Host "System"
+  Write-Host "======================================================================"
+  Write-Host ("BIOS UUID: " + $system.UUID)
+  Write-Host ("BIOS Serial: " + $bios.SerialNumber)
+  Write-Host ("Baseboard Serial: " + $baseboard.SerialNumber)
+  Write-Host "Baseboard Asset Tag: Default string"
+  Write-Host "Enclosure Serial: Default string"
+  Write-Host "Enclosure Asset Tag: Default string"
+  )
 
 
-Network Adapter (NIC)
-======================================================================
-Adapter:                
-Current MAC:            
-Permanent MAC:          
-----------------------------------------------------------------------
-Adapter:                 
-Current MAC:             
-Permanent MAC:           
-----------------------------------------------------------------------
-Adapter:                 
-Current MAC:            
-Permanent MAC:          
-----------------------------------------------------------------------
+$(foreach ($drive in $drives) {
+    $(Write-Host "Disk (HDD/SSD/NVMe)")
+    Write-Host "======================================================================"
+    Write-Host ("Model: " + $drive.Model)
+    Write-Host ("Serial number: " + $drive.SerialNumber)
+    Write-Host "----------------------------------------------------------------------"
+})
+
+$(foreach ($adapter in $adapters) {
+    $(write-host 'Network Adapter (NIC)')
+    Write-Host "======================================================================"
+    Write-Host ("Adapter: " + $adapter.Name)
+    Write-Host ("Current MAC: " + $adapter.MacAddress)
+    # Permanent MAC is typically not available in the default cmdlets
+    Write-Host "Permanent MAC: N/A"
+    Write-Host "----------------------------------------------------------------------"
+})
+
+$(foreach ($module in $memoryModules) {
+    $(write-host 'Physical RAM')
+    Write-Host "======================================================================"
+    Write-Host ("Manufacturer: " + $module.Manufacturer)
+    Write-Host ("Serial: " + $module.SerialNumber)
+    Write-Host ("Asset tag: " + $module.AssetTag)
+    Write-Host "----------------------------------------------------------------------"
+})
 
 
-Physical RAM
-======================================================================
-Manufacturer:            $Manufacturer
-Serial:                  
-Asset tag:               
-----------------------------------------------------------------------
-Manufacturer:            
-Serial:                  
-Asset tag:               
-----------------------------------------------------------------------
 
-
-Power Supply
-======================================================================
-Serial:                  To Be Filled By O.E.M.
-Model part number:       To Be Filled By O.E.M.
-Asset tag number:        To Be Filled By O.E.M.
-----------------------------------------------------------------------
-
+$(
+Write-Host "Power Supply"
+Write-Host "======================================================================"
+Write-Host "Serial:                  To Be Filled By O.E.M."
+Write-Host "Model part number:       To Be Filled By O.E.M."
+Write-Host "Asset tag number:        To Be Filled By O.E.M."
+Write-Host "----------------------------------------------------------------------"
+)
 
 Press any key to continue . . .
 
