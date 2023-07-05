93d1243365f31808625dcb4f27919e175782d740
Calvin <74103103+Calvindd2f@users.noreply.github.com>
2023-07-05 19:07:52 +0100

    Update Get-HWID.psm1

--- Get-HWID.psm1
+++ Get-HWID.psm1
@@ -16,47 +16,47 @@ $(Write-Host " ")
 $(Write-Host "System")
 $(Write-Host "======================================================================")
 $( 
-  Write-Host ("BIOS UUID: " + $system.UUID)
-  Write-Host ("BIOS Serial: " + $bios.SerialNumber)
-  Write-Host ("Baseboard Serial: " + $baseboard.SerialNumber)
-  Write-Host "Baseboard Asset Tag: Default string"
-  Write-Host "Enclosure Serial: Default string"
-  Write-Host "Enclosure Asset Tag: Default string"
+  Write-Host ("BIOS UUID:`t`t " + $system.UUID)
+  Write-Host ("BIOS Serial:`t`t " + $bios.SerialNumber)
+  Write-Host ("Baseboard Serial:`t`t " + $baseboard.SerialNumber)
+  Write-Host "Baseboard Asset Tag:`t`t Default string"
+  Write-Host "Enclosure Serial:`t`t Default string"
+  Write-Host "Enclosure Asset Tag:`t`t Default string"
   )
   $(Write-Host " ")    
   $(Write-Host "Disk (HDD/SSD/NVMe)")
   $(Write-Host "======================================================================")
 $(foreach ($drive in $drives) {
-    Write-Host ("Model: " + $drive.Model)
-    Write-Host ("Serial number: " + $drive.SerialNumber)
+    Write-Host ("Model:`t`t " + $drive.Model)
+    Write-Host ("Serial number:`t`t " + $drive.SerialNumber)
     Write-Host "----------------------------------------------------------------------"
 })
 $(Write-Host " ")  
 $(write-host 'Network Adapter (NIC)')
 $(Write-Host "======================================================================")
 $(foreach ($adapter in $adapters) {
-    Write-Host ("Adapter: " + $adapter.Name)
-    Write-Host ("Current MAC: " + $adapter.MacAddress)
+    Write-Host ("Adapter:`t`t " + $adapter.Name)
+    Write-Host ("Current MAC:`t`t " + $adapter.MacAddress)
     # Permanent MAC is typically not available in the default cmdlets
-    Write-Host "Permanent MAC: N/A"
+    Write-Host "Permanent MAC:`t`t N/A"
     Write-Host "----------------------------------------------------------------------"
 })
 $(Write-Host " ")  
 $(write-host 'Physical RAM')
 $(Write-Host "======================================================================")
 $(foreach ($module in $memoryModules) {
-    Write-Host ("Manufacturer: " + $module.Manufacturer)
-    Write-Host ("Serial: " + $module.SerialNumber)
-    Write-Host ("Asset tag: " + $module.AssetTag)
+    Write-Host ("Manufacturer:`t`t " + $module.Manufacturer)
+    Write-Host ("Serial:`t`t " + $module.SerialNumber)
+    Write-Host ("Asset tag:`t`t " + $module.AssetTag)
     Write-Host "----------------------------------------------------------------------"
 })
 $(Write-Host " ")               
 $(
 Write-Host "Power Supply"
 Write-Host "======================================================================"
-Write-Host "Serial:                  To Be Filled By O.E.M."
-Write-Host "Model part number:       To Be Filled By O.E.M."
-Write-Host "Asset tag number:        To Be Filled By O.E.M."
+Write-Host "Serial:                 To Be Filled By O.E.M."
+Write-Host "Model part number:      To Be Filled By O.E.M."
+Write-Host "Asset tag number:       To Be Filled By O.E.M."
 Write-Host "----------------------------------------------------------------------"
 )
 $(Write-Host " ")       
